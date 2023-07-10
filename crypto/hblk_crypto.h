468f5fe0f7dd4266b869d423a7d26bb0a752780c
Anna78990 <takasuanna1990@gmail.com>
2023-07-10 07:29:10 +0000

    ec create

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -3,8 +3,14 @@
 
 #include <stdint.h>
 #include <openssl/sha.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+
 
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_create(void);
 
 #endif
