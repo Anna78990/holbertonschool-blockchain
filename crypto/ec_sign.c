127da0f7c3eac5ae33bbaed3045208b807103237
Anna78990 <takasuanna1990@gmail.com>
2023-07-13 07:48:37 +0000

    initialize siglen by 0

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -13,17 +13,14 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 		size_t msglen, sig_t *sig)
 {
-	uint32_t siglen;
+	uint32_t siglen = 0;
 
 	if (!key || !msg || !sig)
 		return (NULL);
 
 	bzero(sig->sig, sizeof(sig->sig));
 	if (ECDSA_sign(0, msg, msglen, sig->sig, &siglen, (EC_KEY *)key) != 1)
-	{
-		sig->len = 0;
 		return (NULL);
-	}
-	sig->len = (uint8_t)siglen;
+	sig->len = siglen;
 	return (sig->sig);
 }
