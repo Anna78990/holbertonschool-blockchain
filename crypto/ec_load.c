6b68afcec750dfa9364293c59fee4de809d4266b
Anna78990 <takasuanna1990@gmail.com>
2023-07-13 14:28:39 +0000

    pub -> priv

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -20,27 +20,20 @@ EC_KEY *ec_load(char const *folder)
 	snprintf(key_path, BUFSIZ, "%s/%s", folder, PRI_FILENAME);
 	snprintf(pub_key_path, BUFSIZ, "%s/%s", folder, PUB_FILENAME);
 
-	fp = fopen(key_path, "r");
-	if (!fp)
-		return (NULL);
-
-	key = PEM_read_ECPrivateKey(fp, &key, NULL, NULL);
-	fclose(fp);
-	if (!key)
-		return (NULL);
 	fp = fopen(pub_key_path, "r");
 	if (!fp)
-	{
-		EC_KEY_free(key);
 		return (NULL);
-	}
 
-	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
-	{
-		fclose(fp);
-		EC_KEY_free(key);
+	key = PEM_read_EC_PUBKEY(fp, &key, NULL, NULL);
+	if (!key)
 		return (NULL);
-	}
+	fclose(fp);
+	fp = fopen(pub_key_path, "r");
+	if (!fp)
+		return (NULL);
+	key = PEM_read_ECPrivateKey(fp, &key, NULL, NULL);
+	if (!key)
+		return (NULL);
 
 	fclose(fp);
 
