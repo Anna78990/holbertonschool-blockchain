23baa005ec95ce6d85f8522b8151588159fc5459
Anna78990 <takasuanna1990@gmail.com>
2024-03-14 14:29:06 +0000

    block->info --> block

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -10,7 +10,7 @@
 uint8_t *block_hash(block_t const *block,
 		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	if (!block || !hash_buf)
+	if (!block)
 		return (NULL);
 	sha256((const int8_t *)&block,
 		    sizeof(block->info) + block->data.len, hash_buf);
