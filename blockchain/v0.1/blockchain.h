3bb85b90ec880547df69a7d8c51a8de665213f48
Anna78990 <takasuanna1990@gmail.com>
2024-03-14 16:02:37 +0000

    header file

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -33,6 +33,15 @@ typedef struct block_data_s
     uint32_t    len;
 } block_data_t;
 
+typedef struct block_info_s
+{
+	uint32_t index;
+	uint32_t difficulty;
+	uint64_t timestamp;
+	uint64_t nonce;
+	uint8_t	 prev_hash[SHA256_DIGEST_LENGTH];
+} block_info_t;
+
 /**
  * struct block_s - Block structure
  *
