e3ce74fb44039035fbb55ef17219316650d32fef
Anna78990 <takasuanna1990@gmail.com>
2024-03-22 15:28:46 +0000

    -

---
+++ blockchain/v0.2/block_mine.c
@@ -0,0 +1,26 @@
+#include "blockchain.h"
+
+/**
+ * block_mine - Mines a Block in order to insert it in the Blockchain
+ * @block: pointer to the Block to be mined
+ */
+void block_mine(block_t *block)
+{
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH];
+	uint64_t i, nonce;
+
+	nonce = block->info.nonce;
+
+	for (i = 0; i < ULONG_MAX; i++)
+	{
+		block->info.nonce = i;
+
+		block_hash(block, hash_buf);
+		if (hash_matches_difficulty(hash_buf, block->info.difficulty))
+		{
+			memcpy(block->hash, hash_buf, SHA256_DIGEST_LENGTH);
+			return;
+		}
+	}
+	block->info.nonce = nonce;
+}
--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -6,6 +6,9 @@
 #include <llist.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <limits.h>
+#include <string.h>
+
 #define CHECK_ENDIAN(x) (endian ? SWAPENDIAN(x) : (void)0)
 
 /**
@@ -86,5 +89,7 @@ int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
 int block_is_valid(block_t const *block, block_t const *prev_block);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty);
+void block_mine(block_t *block);
+
 
 #endif
