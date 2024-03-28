bd69c2875fe9673a3a710a63f3b1368820e6ab3b
Anna78990 <takasuanna1990@gmail.com>
2024-03-28 16:10:41 +0000

    blockchain create

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -16,9 +16,17 @@ blockchain_t *blockchain_create(void)
 		     "\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f"
 		     "\x04\x51\x58\x03";
 
+	blockchain = calloc(1, sizeof(blockchain_t));
+
+	if (!blockchain)
+		return (NULL);
+	chain = llist_create(MT_SUPPORT_TRUE);
+	if (!chain)
+		return (free(blockchain), NULL);
+	blockchain->chain = chain;
 	genesis = calloc(1, sizeof(block_t));
 	if (!genesis)
-		return (NULL);
+		return (free(chain), free(blockchain), NULL);
 	genesis->info.index = 0;
 	genesis->info.difficulty = 0;
 	genesis->info.timestamp = 1537578000;
@@ -26,17 +34,10 @@ blockchain_t *blockchain_create(void)
 	memset(genesis->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
 	memcpy(genesis->data.buffer, "Holberton School", 16);
 	genesis->data.len = 16;
-	memcpy(&(genesis->hash), hash, SHA256_DIGEST_LENGTH);
+	memcpy(genesis->hash, hash, SHA256_DIGEST_LENGTH);
 
-	chain = llist_create(MT_SUPPORT_FALSE);
-	if (!chain)
-		return (free(genesis), NULL);
 	if (llist_add_node(chain, genesis, ADD_NODE_FRONT) == -1)
-		return (free(genesis), free(chain), NULL);
+		return (free(genesis), free(chain), free(blockchain), NULL);
 
-	blockchain = calloc(1, sizeof(blockchain_t));
-	if (!blockchain)
-		return (free(genesis), free(chain), NULL);
-	blockchain->chain = chain;
 	return (blockchain);
 }
