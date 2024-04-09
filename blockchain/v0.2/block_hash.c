fd1e58636b9c3fbd7eacabfa06df2787d0bb0cff
Anna78990 <takasuanna1990@gmail.com>
2024-04-09 12:56:41 +0000

    block_hash

--- blockchain/v0.2/block_hash.c
+++ blockchain/v0.2/block_hash.c
@@ -8,10 +8,33 @@
  * Return: pointer to hash_buf on success, otherwise NULL
  */
 uint8_t *block_hash(block_t const *block,
-		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	if (!block)
+	size_t length = (sizeof(block->info) + block->data.len);
+	uint8_t *temp_buf = NULL;
+
+	if (!block || !hash_buf)
 		return (NULL);
-	return (sha256((int8_t const *)block,
-		    sizeof(block->info) + block->data.len, hash_buf));
+	if (length > SHA256_DIGEST_LENGTH)
+	{
+		temp_buf = (uint8_t *)malloc(length);
+		if (temp_buf == NULL)
+			return (NULL);
+		memcpy(temp_buf, (uint8_t *)&block->info, sizeof(block->info));
+		memcpy(temp_buf + sizeof(block->info), block->data.buffer, block->data.len);
+		SHA256(temp_buf, length, hash_buf);
+		free(temp_buf);
+	}
+	else
+	{
+		length = SHA256_DIGEST_LENGTH;
+		temp_buf = (uint8_t *)alloca(length);
+		if (temp_buf == NULL)
+			return (NULL);
+		memcpy(hash_buf, (uint8_t *)&block->info, sizeof(block->info));
+		memcpy(hash_buf + sizeof(block->info), block->data.buffer, block->data.len);
+		SHA256(temp_buf, length, hash_buf);
+		free(temp_buf);
+	}
+	return (hash_buf);
 }
